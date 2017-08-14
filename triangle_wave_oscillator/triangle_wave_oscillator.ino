6f50c5735e1d9ba747945bac759ec1db0c0d03a1
Andy Doswell <andydoswell@hotmail.com>
2017-08-14 12:02:06 +0100

    Add files via upload

---
+++ triangle_wave_oscillator/triangle_wave_oscillator.ino
@@ -0,0 +1,35 @@
+/* Simple low frequency triangle wave generator
+
+  Output PWM on pin 9 to LPF.
+  Pin 7 has a switch connected to ground to select frequency , approx 0.4Hz or 0.04 Hz
+  Pin 8 is high for 0.04Hz, low for 0.4Hz
+  Frequency switch is only checked at start up. A reset will be required to change frequency.
+
+*/
+
+int period;
+
+void setup() {
+  pinMode (9, OUTPUT);
+  pinMode (7, INPUT_PULLUP);
+  pinMode (8, OUTPUT);
+
+  if (digitalRead (7) == true) {
+    period = 50;
+    digitalWrite (8, HIGH);
+  }
+  else {
+    period = 5;
+    digitalWrite (8, LOW);
+  }
+}
+void loop() {
+  for (int i = 0; i < 256; i++) {
+    analogWrite (9, i);
+    delay (period);
+  }
+  for (int i = 255; i > 1; i--) {
+    analogWrite (9, i);
+    delay (period);
+  }
+}
